e501940146f0838cff38a42fabd31839f226b6f7
uraway <masato.uraway@gmail.com>
2021-02-16 19:42:49 +0900

    Update git-update.sh

--- bin/git-update.sh
+++ bin/git-update.sh
@@ -1,15 +1,23 @@
-#!/bin/sh -e
+#!/bin/sh
 
-if [ -n "$1" ]; then
-  master=$1
-else
-  master=`git remote show origin | grep 'HEAD branch:' | cut -d : -f 2 | tr -d '[[:space:]]'`
+user=`git config github.user`
+git remote show ${user} > /dev/null 2>&1
+has_repo=$?
+
+git fetch --all --prune
+modified=`git status | grep modified | wc -l`
+if [ $modified -gt 0 ] ; then git stash ; fi
+branch=`git branch | grep '*' | cut -f 2 -d ' '`
+default_branch=`git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'`
+git checkout $default_branch
+git pull --rebase origin $default_branch
+if [ $has_repo = 0 ] ; then git push ${user} $default_branch; fi
+if [ $branch != $default_branch ] ; then
+    if [ `git branch --merged | grep ${branch} | wc -l` -eq 1 ]
+    then
+        git branch -d ${branch}
+    else
+        git checkout ${branch}
+    fi
 fi
-
-current=`git branch | grep '*' | cut -f 2 -d ' '`
-
-git checkout $master
-git pull origin $master --ff-only
-
-git checkout $current
-git rebase $master
+if [ $modified -gt 0 ] ; then git stash pop ; fi
